1c7d76fca41be7c0c3213786f126ee91d4d22729
baich <baichen7788@gmail.com>
2019-05-27 17:13:46 +0800

    implement rilling forecast ARIMA

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -4,14 +4,20 @@ Created on Sun May 26 18:30:01 2019
 
 @author: baichen
 """
-
+import numpy as np
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from statsmodels.tsa.stattools import acf,pacf
+import statsmodels.tsa.stattools as ts
+from statsmodels.tsa.arima_model import ARIMA
 
-EURUSD=pd.read_csv('EURUSD30.csv',index_col=0,parse_dates=True)['Close']
-print(EURUSD)
+
+EURUSD=pd.read_csv('EURUSD60.csv',index_col=0,parse_dates=True)
+train_EURUSD=EURUSD.iloc[:4000,:]['Close']
+test_EURUSD=EURUSD.iloc[4001:,:]['Close']
+
 start=pd.to_datetime('2014-6-1')
 end=pd.to_datetime('2019-5-25')
 s='AMZN'
@@ -56,19 +62,80 @@ class KalmanFilter(object):
 
         self.current_prob_estimate=(1-kalman_gain*self.c)*predicted_prob_estimate
 
+#acf
+acf1=acf(train_EURUSD)
+acf1=pd.DataFrame([acf1]).T
+acf1.plot(kind='bar')
+plt.show();
+
+#pacf
+pacf1=pacf(train_EURUSD)
+pacf1=pd.DataFrame([pacf1]).T
+pacf1.plot(kind='bar')
+plt.show();   #AR(1) is concerned
+
+#test 1st differencing acf
+price_diff=train_EURUSD-train_EURUSD.shift()
+price_diff=price_diff.dropna()
+acf1_diff=acf(price_diff)
+acf1_diff=pd.DataFrame([acf1_diff]).T
+acf1_diff.plot(kind='bar')
+plt.show();
+
+#test 1st differencing pacf
+pacf1_diff=pacf(price_diff)
+pacf1_diff=pd.DataFrame([pacf1_diff]).T
+pacf1_diff.plot(kind='bar')
+plt.show();
+
+#arima
+price=train_EURUSD.values
+model=ARIMA(price,order=(0,1,0))
+model_fit=model.fit(disp=0)
+print(model_fit.summary())
+pred_arima=model_fit.predict(7000,typ='levels')
+
+#ARIMA rolling forecast
+X=EURUSD['Close'].values
+size=4000
+train, test = X[0:size], X[size:len(X)]
+history=[x for x in train]
+predicts=[]
+for t in range(len(test)):
+
+    #price=np.append(price,EURUSD['Close'].values[i])
+    model=ARIMA(history,order=(5,1,0))
+    model_fit=model.fit(disp=0)
+    output=model_fit.forecast()
+    yhat=output[0]
+    predicts.append(yhat)
+    obs=test[t]
+    history.append(obs)    #update history data
+    print('predicted=%f, expected=%f' % (yhat, obs))
+
+plt.plot(test)
+plt.plot(predicts,color='red')
+plt.show();
+print('predict',predicts)
+
+
+
+
+
+#kalman filter part
 a=1
 b=0
 c=1
 q=0.00001
 r=1
-x=1
+x=1.24
 p=1
 
 filter=KalmanFilter(a,b,c,x,p,q,r)
 predictions=[]
 estimate=[]
 observe=[]
-for data in EURUSD:
+for data in train_EURUSD:
     filter.process(0,data)
     predictions.append(filter.current_state())
     estimate.append(filter.predicted_state())
@@ -77,9 +144,9 @@ for data in EURUSD:
 predictions=[float(i) for i in predictions]
 estimate=[float(i) for i in estimate]
 observe=[float(i) for i in observe]
-print(predictions)
 
-plt.plot(EURUSD.values)
+
+plt.plot(train_EURUSD.values)
 plt.plot(predictions)
 #plt.plot(estimate)
 #plt.plot(observe)
